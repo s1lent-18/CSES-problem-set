b31a1636a0620fa9b69145c21a58370f351ab369
s1lent-18 <a.j.gharat17@gmail.com>
2026-09-12 01:26:29 +0530

    Add two set solution

---
+++ Introductory_Problems/two_set.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll=long long;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); 
+    ll n;
+    cin >> n;
+    vector<int> sumA;
+    vector<int> sumB;
+    ll target;
+    ll total=n*(n+1)/2;
+    ll current_sum=0;
+    if(total%2 != 0)
+    {
+        cout<<"NO\n";
+    }
+    else
+    {
+        cout<<"YES\n";
+        target=total/2;
+        for(int i=n;i>0;i--)
+        {
+            if(current_sum+i<=target)
+            {
+                sumA.push_back(i);
+                current_sum+=i;
+            }
+            else
+            {
+                sumB.push_back(i);
+            }
+        }
+    }
+    cout<< sumA.size();
+    for(int x:sumA)
+    {
+        cout<<x<<" ";
+    }
+    cout<< sumB.size();
+    for(int x:sumB)
+    {
+        cout<<x<<" ";
+    }
+    return 0;
+}
